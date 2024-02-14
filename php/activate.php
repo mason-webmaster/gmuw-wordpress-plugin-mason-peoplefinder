3ba599e6aa6be4cd5cac0973d6d44a9c6a9279ab
Jan Macario <jmacario@gmu.edu>
2024-02-13 21:36:03 -0500

    Implements the creation of custom student tables on plugin activation.

--- php/activate.php
+++ php/activate.php
@@ -7,19 +7,22 @@
 function gmuw_pf_plugin_activate(){
 
   // Create custom tables
-    //gmuw_pf_create_custom_tables();
+    gmuw_pf_create_custom_tables();
 
 }
 
 function gmuw_pf_create_custom_tables(){
 
   // Create custom tables needed for the plugin
-    // table 1
-      gmuw_pf_create_custom_table_1();
-
+    // students
+      gmuw_pf_create_custom_table_students();
+    // new students
+      gmuw_pf_create_custom_table_students('new');
+    // old students
+      gmuw_pf_create_custom_table_students('old');
 }
 
-function gmuw_pf_create_custom_table_1(){
+function gmuw_pf_create_custom_table_students($table_name_suffix=''){
 
   // Get globals
     global $wpdb;
@@ -28,11 +31,26 @@ function gmuw_pf_create_custom_table_1(){
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
   // Set table name, using the database prefix
-    $table_name = $wpdb->prefix . "gmuw_pf_1";
+    $table_name = $wpdb->prefix . "gmuw_pf_students";
+
+  // Add suffix to events table name if necessary
+    switch ($table_name_suffix) {
+      case 'new':
+        $table_name.='_new';
+        break;
+      case 'old':
+        $table_name.='_old';
+        break;
+    }
 
   // Write SQL statement to create table
     $sql = "CREATE TABLE $table_name (
      ID int(11) NOT NULL AUTO_INCREMENT,
+     import_time int(11) NOT NULL,
+     student_name nvarchar(255) DEFAULT NULL,
+     student_major nvarchar(255) DEFAULT NULL,
+     student_phone_number nvarchar(255) DEFAULT NULL,
+     student_pronouns nvarchar(255) DEFAULT NULL,
      when_created datetime NOT NULL,
      when_modified datetime NOT NULL,
      PRIMARY KEY  (ID)
