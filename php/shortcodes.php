9dc21953f42cdbefd5b4ba60de467acaffca5702
Jan Macario <jmacario@gmu.edu>
2024-04-04 11:47:49 -0400

    Adds new shortcode to output an HTML message indicating whether the user is or is not on the Mason network.

--- php/shortcodes.php
+++ php/shortcodes.php
@@ -16,6 +16,9 @@ add_action('init', function(){
         'gmuw_pf_shortcode' //callback function
     );
 
+    // mason network status?
+        add_shortcode('gmuw_pf_mason_network_status_message', 'gmuw_pf_mason_network_status_message');
+
     // peoplefinder search
         add_shortcode('gmuw_pf_search_form', 'gmuw_pf_search_form');
 
@@ -34,6 +37,20 @@ function gmuw_pf_shortcode(){
 
     // Return value
     return $content;
+
+}
+
+function gmuw_pf_mason_network_status_message(){
+
+  // are we in the Mason IP range?
+  if (gmuw_pf_ip_in_mason_range($_SERVER['REMOTE_ADDR'])) {
+    $content='<p>You are on the Mason network.</p>';
+  } else {
+    $content='<p>You are not on the Mason network.</p>';
+  }
+
+  // Return value
+  return $content;
 
 }
 
